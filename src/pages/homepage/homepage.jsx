cce3c7cb17693b510bb81776dd4864ae7ed4f82b
Shivkumar Jaiswal <shivsj1402@gmail.com>
2020-05-18 19:25:27 -0300

    formating the project structure and styling the hamepage

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from '../../Components/directory-menu/directory-menu';
+
+export const HomePage = () => (
+    <div className='homepage'>
+      <h1>Welcome to my Homepage</h1>
+      <Directory />
+    </div>
+  );
+  
