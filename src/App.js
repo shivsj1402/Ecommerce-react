385fbae18fe98df331c9f54f6112c0b6260bc22e
Shivkumar Jaiswal <shivsj1402@gmail.com>
2020-05-18 18:00:58 -0300

    creating homepage template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HomePage } from './Components/homepage/homepage'
 
 function App() {
   return (
     <div>
-      hello world!
+      <HomePage />
     </div>
   );
 }
