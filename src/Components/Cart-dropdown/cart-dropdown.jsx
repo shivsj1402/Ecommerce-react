483e6c17af591a1b441279e25e18311d15978ef4
Shivkumar Jaiswal <shivsj1402@gmail.com>
2020-05-26 19:54:21 -0300

    added functionality to add items to cart

--- src/Components/Cart-dropdown/cart-dropdown.jsx
+++ src/Components/Cart-dropdown/cart-dropdown.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import './cart-dropdown.scss'
 import CustomButton from '../Custom-button/custom-button';
+import CartItem from '../Cart-tem/cart-item';
 
-function CartDropdown () {
+function CartDropdown ({cartItems}) {
         return(
             <div className="cart-dropdown">
-                <div className="cart-items"></div>
+                <div className="cart-items">
+                {console.log(cartItems)}
+                {cartItems.map(item =><CartItem key={item.id} item={item}/> )}
+                </div>
                 <CustomButton>GO TO CHECKOUT</CustomButton>
             </div>
            
         )
 }
 
-export default CartDropdown;
+const mapStateToProps= state =>({
+    cartItems : state.cart.cartItems
+    })
+
+export default connect(mapStateToProps)(CartDropdown);
