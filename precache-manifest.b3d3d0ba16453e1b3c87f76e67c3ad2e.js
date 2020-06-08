182f8ac38006c645aaeb97cdbf3358590b2a18ac
Shivkumar Jaiswal <shivsj1402@gmail.com>
2020-06-08 17:32:03 -0300

    Updates

---
+++ precache-manifest.b3d3d0ba16453e1b3c87f76e67c3ad2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacf952c3745b4ec0703c480b5483daa",
+    "url": "/Ecommerce-react/index.html"
+  },
+  {
+    "revision": "528ed3a590c559204f98",
+    "url": "/Ecommerce-react/static/css/main.c4e97055.chunk.css"
+  },
+  {
+    "revision": "77057e1bddee50a08b38",
+    "url": "/Ecommerce-react/static/js/2.22a194ea.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/Ecommerce-react/static/js/2.22a194ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "528ed3a590c559204f98",
+    "url": "/Ecommerce-react/static/js/main.6c4a2d22.chunk.js"
+  },
+  {
+    "revision": "256cb721a45895b3fb3c",
+    "url": "/Ecommerce-react/static/js/runtime-main.63df2d09.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Ecommerce-react/static/media/Logo.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Ecommerce-react/static/media/cart-icon.30f07071.svg"
+  }
+]);
